375729347e4b74a89e2673ca895abe9511f28e1c
GGanon <77290884+gganon@users.noreply.github.com>
2022-12-01 11:53:33 +0530

    Solve Day 1

--- src/day1/calories/index.ts
+++ src/day1/calories/index.ts
@@ -1,5 +1,31 @@
 import { readInput } from '../../input';
 
-const input = readInput('day1/calories/calories.txt').toString();
+const input = readInput('day1/calories/calories.txt').toString().trim();
 
-console.log(input);
+const elfCollections = input
+  .split('\n\n')
+  .map((elfBlock) =>
+    elfBlock.split('\n').map((calories) => parseInt(calories))
+  );
+
+let elfWithMostCalories = {
+  elfNumber: 0,
+  totalCalories: -Infinity,
+};
+
+for (const [elfNumber, items] of elfCollections.entries()) {
+  const totalCalories = items.reduce((total, calories) => total + calories, 0);
+  console.log(
+    `Elf ${elfNumber + 1} is carrying a total of ${totalCalories} calories`
+  );
+
+  if (elfWithMostCalories.totalCalories < totalCalories) {
+    elfWithMostCalories = { elfNumber, totalCalories };
+  }
+}
+
+console.log(
+  `\n\nElf ${
+    elfWithMostCalories.elfNumber + 1
+  } is carrying the most calories: ${elfWithMostCalories.totalCalories}`
+);
